507c2605cb3b26606a0d8d0d6c8e12922fae62bc
MiloCubed <135522693+MiloCubed@users.noreply.github.com>
2024-02-28 12:14:49 +0800

    Change archive.today url from latest to newest I originally just guessed "latest" but the [official FAQ](https://archive.today/faq) states to use "newest" instead so I'd rather go with the official wording.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 browser.browserAction.onClicked.addListener(tab => {
     browser.tabs.create({
-      url: "https://archive.today/latest/" + tab.url,
+      url: "https://archive.today/newest/" + tab.url,
     });
 })
